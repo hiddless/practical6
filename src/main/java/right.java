bedf6e622366123e8b286aab2be9d5d398776e04
hiddless <christophertheodorepayne@gmail.com>
2025-10-21 21:26:54 +0300

    task6

--- src/main/java/right.java
+++ src/main/java/right.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class right {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("lenht: ");
+        int lenht = scn.nextInt();
+        int[]duru= new int[lenht];
+
+        for(int i=0;i<lenht;i++){
+            System.out.print("duru["+i+"]: ");
+            duru[i]=scn.nextInt();
+        }
+        System.out.print("n: ");
+        int n = scn.nextInt();
+        int[]newduru= new int[lenht];
+        for(int i=lenht-1;i>=n;i--){
+            newduru[i]=duru[i-n];
+        }
+        for(int i=0;i<n&&i<lenht;i++){
+            newduru[i]=0;
+        }
+
+        System.out.print("duru={ ");
+        for(int i=0;i<lenht;i++){
+            System.out.print(newduru[i]);
+            if (i < lenht - 1) System.out.print(" ");
+        }
+        System.out.print(" }");
+
+    }
+
+
+
 }
--- src/main/java/shiftingarrayleft.java
+++ src/main/java/shiftingarrayleft.java
@@ -1,29 +1,5 @@
 import java.util.Scanner;
 
 public class shiftingarrayleft {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("lenght: ");
-        int lenght = scn.nextInt();
-
-        int[]duru= new int[lenght];
-        for(int i=0;i<lenght;i++){
-            System.out.print("duru["+ i +"] = ");
-            duru[i]=scn.nextInt();
-        }
-        System.out.print("n: ");
-        int left=scn.nextInt();
-        left=left%lenght;
-        for(int a=0;a<lenght;a++){
-            int first=duru[a];
-            for(int i=0;i<lenght-1;i++){
-                duru[i]=duru[i+1];
-            }
-            duru[lenght-1]=first;
-        }
-        System.out.println("new: ");
-        for(int num:duru) System.out.print(num+" ");
-
-    }
 
 }
