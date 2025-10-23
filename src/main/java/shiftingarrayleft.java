cf4cb24ba03ce41eee41e905aa7157529ac12ca2
hiddless <christophertheodorepayne@gmail.com>
2025-10-23 12:56:24 +0300

    task6

--- src/main/java/shiftingarrayleft.java
+++ src/main/java/shiftingarrayleft.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 
 public class shiftingarrayleft {
-
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("lenght= ");
+        int lenght = scn.nextInt();
+        int [] duru = new int [lenght];
+        for(int i=0;i<lenght;i++){
+            System.out.print("duru["+i+"]=");
+            duru[i] = scn.nextInt();
+        }
+        System.out.print("n: ");
+        int n = scn.nextInt();
+        if(n>=lenght){
+            for (int i=0;i<lenght;i++) duru[i]=0;
+        }else {
+            for (int i=0;i<lenght-n;i++){
+                duru[i]=duru[i+n];
+            }
+            for (int i=lenght-n;i<lenght;i++){
+                duru[i]=0;
+            }
+        }
+        System.out.print("new={ ");
+        for(int i=0;i<lenght;i++) System.out.print(duru[i]+" ");
+        System.out.println("}");
+    }
 }
